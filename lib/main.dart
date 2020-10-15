213f625aef9baf53a8b6285925236b83f7e319db
ckcharless <57629693+ckcharless@users.noreply.github.com>
2020-10-15 17:54:21 +0700

    ganti icon notif

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
               backgroundColor: Colors.blueGrey
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.),
+              icon: Icon(Icons.notifications_active),
               title: Text('Notification'),
               backgroundColor: Colors.blueGrey
           ),
