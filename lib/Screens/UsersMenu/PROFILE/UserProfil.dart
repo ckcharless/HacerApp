7c61971c6bc173a62d9eceae44b8281940bd78ca
Reynerroxas <72919593+Reynerroxas@users.noreply.github.com>
2021-02-25 01:31:20 +0700

    25/2/2021

--- lib/Screens/UsersMenu/PROFILE/UserProfil.dart
+++ lib/Screens/UsersMenu/PROFILE/UserProfil.dart
@@ -10,10 +10,11 @@ static String routeName = '/profile';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Center(
           child:
-        Text("Profilee"),
+        Text("Profile"),
         ),
         backgroundColor: Colors.white60,
         automaticallyImplyLeading: false,
--- lib/Screens/UsersMenu/PROFILE/commponents/body.dart
+++ lib/Screens/UsersMenu/PROFILE/commponents/body.dart
@@ -1,21 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hacer/Screens/UsersMenu/PROFILE/commponents/profil_pic.dart';
 
 class Body extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return Column(
-     children: [
-       SizedBox(
-         height: 115,
-         width: 115,
-         child: CircleAvatar(
-           backgroundImage: AssetImage("assets/images/fotoprofil.jpg"),
-         ),
-       ),
+    return Column(
+
+      children: [
+        SizedBox(height: 10,),
+        ProfilPic(),
+        SizedBox(height: 10,),
+        ProfilMenu(
+            text: "DERRICK ROSE",
+            icon: "assets/icons/name.svg"),
+
+        ProfilMenu(
+            text: "081234578454",
+            icon: "assets/icons/phone.svg"),
+
+        ProfilMenu(
+            text: "MALE",
+            icon: "assets/icons/gender.svg"),
+
+        ProfilMenu(
+            text: "4 OKTOBER 1988",
+            icon: "assets/icons/birthday.svg"),
+
+        ProfilMenu(
+            text: "derrickrose@gmail.com",
+            icon: "assets/icons/email.svg"),
+      ],
+
+    );
+  }
+}
 
 
-     ],
+class ProfilMenu extends StatelessWidget{
+  const ProfilMenu({
+    Key key,
+    @required this.text,
+    @required this.icon,
+}) : super(key: key);
 
-   );
+  final String text, icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 10),
+      child: FlatButton(
+        padding: EdgeInsets.all(10),
+        shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Color(0xFFF5F6F9),
+        onPressed: () {},
+        child: Row(
+          children: [
+
+            SvgPicture.asset(
+              icon,
+              width: 22,
+              color: Colors.blue,
+            ),
+            SizedBox(width: 20,),
+            Expanded(child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
